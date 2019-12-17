211e77a6cad0d7b634234170faafb040984ae5b8
Juan Hernandez <jhernand@redhat.com>
2019-12-17 18:43:09 +0100

    Add authentication handler
    This patch adds an HTTP handler that checks the JWT tokens present in
    the `Authorization` header.
    
    Signed-off-by: Juan Hernandez <jhernand@redhat.com>

---
+++ authentication/context_test.go
@@ -0,0 +1,71 @@
+/*
+Copyright (c) 2019 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// This file contains tests for the functions that extract authentication and authorization
+// information from contexts.
+
+package authentication
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Add token to context", func() {
+	It("Adds the token", func() {
+		token := IssueToken(nil)
+		ctx := ContextWithToken(context.TODO(), token)
+		extracted := ctx.Value(tokenKeyValue)
+		Expect(extracted).To(BeIdenticalTo(token))
+	})
+})
+
+var _ = Describe("Get token from context", func() {
+	It("Succeeds if there is a token", func() {
+		token := IssueToken(nil)
+		ctx := context.WithValue(context.TODO(), tokenKeyValue, token)
+		extracted, err := TokenFromContext(ctx)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(extracted).ToNot(BeNil())
+		Expect(extracted.Raw).To(Equal(token.Raw))
+	})
+
+	It("Succeeds if there is no token", func() {
+		ctx := context.TODO()
+		extracted, err := TokenFromContext(ctx)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(extracted).To(BeNil())
+	})
+})
+
+var _ = Describe("Get bearer from context", func() {
+	It("Succeeds if there is a token", func() {
+		token := IssueToken(nil)
+		ctx := context.WithValue(context.TODO(), tokenKeyValue, token)
+		extracted, err := BearerFromContext(ctx)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(extracted).To(Equal(token.Raw))
+	})
+
+	It("Succeeds if there is no token", func() {
+		ctx := context.TODO()
+		extracted, err := BearerFromContext(ctx)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(extracted).To(BeEmpty())
+	})
+})
