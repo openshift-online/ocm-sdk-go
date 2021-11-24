8efdfaaeb20a972b56681de6b2926332daccd818
Daniel Berger <djberg96@gmail.com>
2021-11-24 10:16:51 -0500

    Update to model 0.0.155
    The more relevant change in the new version of the model is the addition
    of the `status_board` service.

---
+++ examples/create_product.go
@@ -0,0 +1,80 @@
+/*
+Copyright (c) 2021 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	sdk "github.com/openshift-online/ocm-sdk-go"
+	"github.com/openshift-online/ocm-sdk-go/logging"
+	sb "github.com/openshift-online/ocm-sdk-go/statusboard/v1"
+)
+
+func main() {
+	// Create a context:
+	ctx := context.Background()
+
+	// Create a logger that has the debug level enabled:
+	logger, err := logging.NewGoLoggerBuilder().
+		Debug(true).
+		Build()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't build logger: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Create the connection, and remember to close it:
+	token := os.Getenv("OCM_TOKEN")
+	connection, err := sdk.NewConnectionBuilder().
+		Logger(logger).
+		URL("http://localhost:8000").
+		Tokens(token).
+		BuildContext(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't build connection: %v\n", err)
+		os.Exit(1)
+	}
+	defer connection.Close()
+
+	// Get the client for the resource that manages the collection of products:
+	collection := connection.StatusBoard().V1().Products()
+
+	// Prepare the description of the product to create:
+	product, err := sb.NewProduct().
+		Name("myproduct").
+		Fullname("myproduct's fullname").
+		Build()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't create product description: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Send a request to create the product:
+	response, err := collection.Add().
+		Body(product).
+		SendContext(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't create product: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Print the result:
+	product = response.Body()
+	fmt.Printf("%s - %s\n", product.ID(), product.Name())
+}
---
+++ examples/list_applications.go
@@ -0,0 +1,120 @@
+/*
+Copyright (c) 2021 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// This example shows how to retrieve the collection of applications
+// from the Status Board project.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	sdk "github.com/openshift-online/ocm-sdk-go"
+	"github.com/openshift-online/ocm-sdk-go/logging"
+	sb "github.com/openshift-online/ocm-sdk-go/statusboard/v1"
+)
+
+func main() {
+	// Create a context:
+	ctx := context.Background()
+
+	// Create a logger that has the debug level enabled:
+	logger, err := logging.NewGoLoggerBuilder().
+		Debug(true).
+		Build()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't build logger: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Create the connection, and remember to close it:
+	token := os.Getenv("OCM_TOKEN")
+	connection, err := sdk.NewConnectionBuilder().
+		Logger(logger).
+		URL("http://localhost:8000").
+		Tokens(token).
+		BuildContext(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't build connection: %v\n", err)
+		os.Exit(1)
+	}
+	defer connection.Close()
+
+	// Get the client for the resource that manages the collection of applications:
+	collection := connection.StatusBoard().V1().Applications()
+
+	// Retrieve the list of applications using pages of ten items, till we get a page that has less
+	// items than requests, as that marks the end of the collection:
+	size := 10
+	page := 1
+	for {
+		// Retrieve the page:
+		response, err := collection.List().
+			//Fullname("similitudinem-consentiat"). // Uncomment this to restrict results by fullname
+			Size(size).
+			Page(page).
+			SendContext(ctx)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Can't retrieve page %d: %s\n", page, err)
+			os.Exit(1)
+		}
+
+		// Display the page:
+		response.Items().Each(func(application *sb.Application) bool {
+			fmt.Printf("%s - %s - %s\n", application.ID(), application.Name(), application.Owners())
+			return true
+		})
+
+		// Break the loop if the size of the page is less than requested, otherwise go to
+		// the next page:
+		if response.Size() < size {
+			break
+		}
+		page++
+	}
+
+	// Get a list of services for a given application
+	application_id := "92b631f1-b4a7-47a7-89e4-ea9b26fafebe" // Adjust as needed
+	services_collection := connection.StatusBoard().V1().Applications().Application(application_id).Services()
+	page = 1
+
+	for {
+		// Retrieve the page:
+		response, err := services_collection.List().
+			Size(size).
+			Page(page).
+			SendContext(ctx)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Can't retrieve page %d: %s\n", page, err)
+			os.Exit(1)
+		}
+
+		// Display the page:
+		response.Items().Each(func(service *sb.Service) bool {
+			fmt.Printf("%s - %s\n", service.ID(), service.Name())
+			return true
+		})
+
+		// Break the loop if the size of the page is less than requested, otherwise go to
+		// the next page:
+		if response.Size() < size {
+			break
+		}
+		page++
+	}
+}
