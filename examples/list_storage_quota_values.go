d6662dfa3e339336e2459a413289c5b867500131
Alba Hita Catala <ahitacat@redhat.com>
2024-04-10 15:51:35 +0200

    chore: update model to v0.0.367
    Signed-off-by: Alba Hita Catala <ahitacat@redhat.com>

---
+++ examples/list_storage_quota_values.go
@@ -0,0 +1,69 @@
+/*
+Copyright (c) 2024 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// This example shows how to retrieve the collection of versions.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	sdk "github.com/openshift-online/ocm-sdk-go"
+	v1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
+	"github.com/openshift-online/ocm-sdk-go/logging"
+)
+
+func main() {
+	// Create a context:
+	ctx := context.Background()
+
+	// Create a logger that has the debug level enabled:
+	logger, err := logging.NewGoLoggerBuilder().
+		Debug(true).
+		Build()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't build logger: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Create the connection, and remember to close it:
+	token := os.Getenv("OCM_TOKEN")
+	connection, err := sdk.NewConnectionBuilder().
+		Logger(logger).
+		Tokens(token).
+		BuildContext(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't build connection: %v\n", err)
+		os.Exit(1)
+	}
+	defer connection.Close()
+
+	// Get the storage quota values
+	storageResponse, err := connection.ClustersMgmt().V1().StorageQuotaValues().List().Send()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't retrieve storage quota values: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Display the values
+	storageResponse.Items().Each(func(l *v1.StorageQuota) bool {
+		fmt.Printf("%v - %v\n", l.Unit(), l.Value())
+		return true
+	})
+}
